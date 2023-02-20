4e466a613322afeeeba318bf15293138ffd48368
Adejola Samuel <adejolasamuel77@gmail.com>
2023-02-20 06:47:00 -0800

    Hello, Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - A program that prints a line with puts function 
- *Hello C
+ * Return: 0 (Success)
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual")
+puts("\"Programming is like building a multilingual");
 return(0);
 }
